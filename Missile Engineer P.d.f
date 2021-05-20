0fd465a25257f35b9c563ca429999fa0e687d9f6
Aditya <balaaditya2902@gmail.com>
2021-05-20 21:51:07 +0530

    Add files via upload

---
+++ Missile Engineer P.d.f
@@ -0,0 +1,18 @@
+                                                     Requirement Of Missile Engineer
+
+ Qualifications Needed:
+
+1. Bachelors Degree In Mechanical Engineering/Electrical Engineering.
+
+2. PG in Air Armament Engineering/PG in Combat Vehicles Engineering/PG in Guided Missiles     Engineering/PG in Microwave and Radar Engineering.
+
+3. PhD In Missile Engineering.
+
+4.5-Years Work Experience In Army/Military/DRDO.
+
+
+  Health Status:
+
+1. Physical Health Certificate.
+
+2. Psychic Health Certificate.
---
+++ myscript.js
@@ -0,0 +1,6 @@
+function access(){
+  alert ("Press OK If You Want To Login");
+}
+function access2(){
+  alert ("Press OK If You Want To Submit Your Details")
+}
